f2ddaa5f4bc6cd6647c6b4ae19cbc4a1a8a98633
Débora Bucci <dbucci@protonmail.com>
2021-12-22 16:38:08 -0300

    Add classes names to div.

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,10 @@
-import react, { Component } from "react";
+import React, { Component } from "react";
 import "./Hello.css";
 
 class Hello extends Component {
   render() {
     return (
-      <div>
+      <div className="f1 tc">
         <h1>Hello World</h1>
         <p>Welcome to React</p>
       </div>
