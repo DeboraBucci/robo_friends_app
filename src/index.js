64979f0e3c33f9b71192e9b6629714f9da76ef7c
Débora Bucci <dbucci@protonmail.com>
2021-12-22 18:00:48 -0300

    Render CadList component with 'robots' as property.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Card from "./Card";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
-import robots from "./robots";
+import { robots } from "./robots";
+import CardList from "./CardList";
 
-ReactDOM.render(<Card />, document.getElementById("root"));
+ReactDOM.render(<CardList robots={robots} />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
