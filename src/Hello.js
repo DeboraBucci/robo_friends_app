3c99ddcf4dec8fc5880f9468707c1f76e7b7c221
Débora Bucci <dbucci@protonmail.com>
2021-12-22 16:14:16 -0300

    New class component Hello created.

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import react, { Component } from "react";
+import "./Hello.css";
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <p>Welcome to React</p>
+      </div>
+    );
+  }
+}
+
+export default Hello;
