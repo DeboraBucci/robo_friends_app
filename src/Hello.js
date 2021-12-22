b64e08c5f3d60284c49a11afe4dd9d2c6bcff567
Débora Bucci <dbucci@protonmail.com>
2021-12-22 16:40:17 -0300

    Add comment of how the component would be if it were a function.

--- src/Hello.js
+++ src/Hello.js
@@ -12,4 +12,14 @@ class Hello extends Component {
   }
 }
 
+/* As a function:
+const Hello = props => {
+  return (
+    <div className="f1 tc">
+        <h1>Hello World</h1>
+        <p>{props.greeting}</p>
+      </div>
+  )
+}
+*/
 export default Hello;
