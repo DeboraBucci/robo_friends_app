a2decb343f60c2fce79068826a30c508b2528420
Debora Bucci <dbucci@protonmail.com>
2022-06-01 16:02:41 -0300

    replace the use of states using redux to manage the changes inside the app

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,13 +4,24 @@ import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/Scroll.js";
 import ErrorBoundary from "../components/ErrorBoundary";
+import { connect } from "react-redux";
+import { setSearchField } from "../actions";
+
+const mapStateToProps = (state) => ({
+  searchField: state.searchField,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  onSearchChange: (event) => {
+    dispatch(setSearchField(event.target.value));
+  },
+});
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       robots: [],
-      searchfield: "",
     };
   }
 
@@ -20,16 +31,11 @@ class App extends Component {
       .then((users) => this.setState({ robots: users }));
   }
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
-
   render() {
-    const { robots, searchfield } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter((robot) => {
-      return robot.name
-        .toLocaleLowerCase()
-        .includes(searchfield.toLocaleLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     });
 
     return !robots.length ? (
@@ -37,7 +43,7 @@ class App extends Component {
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <ErrorBoundary>
             <CardList robots={filteredRobots} />
@@ -48,4 +54,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
