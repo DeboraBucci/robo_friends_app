a0812d6e264605622b830fbf49b2c31328b854d5
Debora Bucci <dbucci@protonmail.com>
2022-06-01 16:17:16 -0300

    delete 'webVitals' report

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { applyMiddleware, createStore } from "redux";
 import App from "./containers/App";
 import "./index.css";
 import "tachyons";
-import reportWebVitals from "./reportWebVitals";
 import { searchRobots } from "./reducers";
 import { createLogger } from "redux-logger";
 
@@ -18,8 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
