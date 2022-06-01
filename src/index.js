93b5a2e05fca4ff8f781ef896beba8e5e156b316
Debora Bucci <dbucci@protonmail.com>
2022-06-01 16:40:29 -0300

    fetch the robots using 'redux' with the help of 'redux-thunk'

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { applyMiddleware, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import App from "./containers/App";
 import "./index.css";
 import "tachyons";
-import { searchRobots } from "./reducers";
+import { searchRobots, requestRobots } from "./reducers";
 import { createLogger } from "redux-logger";
-
+import thunkMiddleware from "redux-thunk";
 const logger = createLogger();
-const store = createStore(searchRobots, applyMiddleware(logger));
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware, logger)
+);
 
 ReactDOM.render(
   <Provider store={store}>
